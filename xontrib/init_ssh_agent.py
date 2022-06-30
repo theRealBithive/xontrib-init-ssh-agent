35c35f89a3b32b137626fd9e6a03d11aac65e36c
Bithive <bithive@bithive.space>
2022-06-30 13:52:52 +0200

    adds SSH_AUTH_SOCK and SSH_AGENT_PID to the __xonsh__.env after startinx ssh-agent

--- xontrib/init_ssh_agent.py
+++ xontrib/init_ssh_agent.py
@@ -1,19 +1,17 @@
-
-from xonsh.built_ins import XSH
-
+import subprocess
 
 __all__ = ()
 
-print('This is xontrib-init-ssh-agent!')
+# Launches the ssh-agent
+output = subprocess.check_output('ssh-agent')
 
+output = output.decode("utf-8")
+output = output.split(';')
+# Split the output on ; which seperates the commands resulting in a List that looks like this:
+# ['SSH_AUTH_SOCK=/tmp/ssh-XXXXXXIOvdyN/agent.20259', ' export SSH_AUTH_SOCK', '\nSSH_AGENT_PID=20260', ' export SSH_AGENT_PID', '\necho Agent pid 20260', '\n']
 
-# Good start! Get more documentation -> https://xon.sh/contents.html#guides
+SSH_AUTH_SOCK = output[0].split('=')[1]
+SSH_AGENT_PID = output[2].split('=')[1]
 
-# accessing environment variables
-
-# Some code in Using Python API:
-var = XSH.env.get("VAR", "default")
-
-
-result = __xonsh__.subproc_captured_stdout(['echo', '1'])
-
+__xonsh__.env['SSH_AUTH_SOCK'] = SSH_AUTH_SOCK
+__xonsh__.env['SSH_AGENT_PID'] = SSH_AGENT_PID
